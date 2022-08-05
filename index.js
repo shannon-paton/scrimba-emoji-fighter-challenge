9c0b5477aa7919c6aebed1e73b1bff3f3d4f4077
shannon-paton <79596670+shannon-paton@users.noreply.github.com>
2022-08-04 22:41:06 -0400

    get length of array when button is clicked

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ let stageEl = document.getElementById("stage")
 let fightButton = document.getElementById("fightButton")
 
 fightButton.addEventListener("click", function() {
+    arrayLength = fighters.length
+    console.log(arrayLength)
+
     // Challenge:
     // When the user clicks on the "Pick Fighters" button, pick two random 
     // emoji fighters and display them as i.e. "🦀 vs 🐢" in the "stage" <div>.
