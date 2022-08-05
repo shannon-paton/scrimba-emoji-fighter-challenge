a6e3d770b951b2fe0fa7bcc76ce98cdba49f4685
shannon-paton <79596670+shannon-paton@users.noreply.github.com>
2022-08-04 23:08:19 -0400

    refactored to use only javascript file

--- index.js
+++ index.js
@@ -9,14 +9,13 @@ fightButton.addEventListener("click", function() {
     arrayLength = fighters.length
 
     // get random number from 0 to array length
+    stage.innerHTML = `? vs ?`
+
     let randomNumber1 = Math.floor(Math.random() * arrayLength);
-    player1.innerHTML = fighters[randomNumber1]
-
-        // get random number from 0 to array length
     let randomNumber2 = Math.floor(Math.random() * arrayLength);
-    player2.innerHTML = fighters[randomNumber2]
 
-    // Challenge:
-    // When the user clicks on the "Pick Fighters" button, pick two random 
-    // emoji fighters and display them as i.e. "🦀 vs 🐢" in the "stage" <div>.
+    let player1Fighter = fighters[randomNumber1]
+    let player2Fighter = fighters[randomNumber2]
+
+    stage.innerHTML = `${player1Fighter} vs ${player2Fighter}`
 })
